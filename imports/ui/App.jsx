32b508d31ec2d1016a3e33cec3bb92251d77035b
Taoka Yuji <mpr1q7z@gmail.com>
2019-12-28 22:35:18 +0900

    Todo Listを表示するように修正

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import React, { Component } from 'react';
+import Task from './Task.jsx';
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
-);
+export default class App extends Component {
+  getTasks() {
+    return [
+      { _id: 1, text: 'This is task 1' },
+      { _id: 2, text: 'This is task 2' },
+      { _id: 3, text: 'This is task 3' },
+    ];
+  }
 
-export default App;
+  renderTask() {
+    return this.getTasks().map((task) => (
+      <Task key={task._id} task={task.text} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+        <ul>
+          {this.renderTask()}
+        </ul>
+      </div>
+    );
+  }
+}
